9724654398de5dd7b62a45184b6379e73fb24e9e
luoranyou <52059700+luoranyou@users.noreply.github.com>
2024-10-28 19:42:43 +0800

    Add files via upload

---
+++ 百度页面/index.js
@@ -0,0 +1,11 @@
+moretrigger.onmouseover = function () { document.getElementById('menu').style.display = 'block'; };
+moretrigger.onmouseover = function () { document.getElementById('menu').style.display = 'block'; };
+menu.onmouseout = function () { document.getElementById('menu').style.display = 'none'; };
+
+settingsStrigger.onmouseover = function () { document.getElementById('settings').style.display = 'block'; };
+settings.onmouseout = function () { document.getElementById('settings').style.display = 'none'; };
+
+
+weatherrigger1.onmouseover = function () { document.getElementById('weather').style.display = 'block'; };
+weatherrigger2.onmouseover = function () { document.getElementById('weather').style.display = 'block'; };
+weather.onmouseout = function () { document.getElementById('weather').style.display = 'none'; };  
